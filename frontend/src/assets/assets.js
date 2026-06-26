4fc43c17524f8905f9e26226768d16204642a47e
UmeHania07 <umehania.shahid@gmail.com>
2026-06-26 23:53:23 +0500

    update

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -2,8 +2,6 @@
 // src/assets/assets.js
 
 import logo from './logo.png'
-import hero1 from './hero1.png'
-import battery from './battery.avif'
 import exchange_icon from './exchange_icon.png'
 import quality_icon from './quality_icon.png'
 import support_img from './support_img.png'
@@ -12,13 +10,9 @@ import about_img from './about_img.png'
 import contact_img from './contact_img.png'
 import statesection from './statesection.png'
 import map from './map.webp'
-import map2 from './map2.png'
-
 
 export const assets = {
   logo,
-  hero1,
-  battery,
   exchange_icon,
   quality_icon,
   support_img,
@@ -27,6 +21,5 @@ export const assets = {
   contact_img,
   statesection,
   map,
-  map2,
 
 }
